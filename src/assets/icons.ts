c71bb13d13c4265a7e1484a21006a0db8b004efc
LilLakes <laghetti.francesco.dev@gmail.com>
2025-02-24 14:52:46 +0100

    Fixed tsconfig Added icons in assets and public

---
+++ src/assets/icons.ts
@@ -0,0 +1,5 @@
+export const reactIcon = "/react_logo.png";
+export const reduxIcon = "/redux_logo.svg";
+export const typescriptIcon = "/typescript_logo.png";
+export const viteIcon = "/vite_logo.svg";
+export const vitestIcon = "/vitest_logo.png";
--- src/components/containers/TestComponent/TestComponent.tsx
+++ src/components/containers/TestComponent/TestComponent.tsx
@@ -1,9 +1,20 @@
+import "./TestComponent.css"
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/rootReducer";
 import { decrement, decrementAsync, increment, incrementAsync } from "@/store/counter/counterSlice";
+import { reactIcon, reduxIcon, typescriptIcon, viteIcon, vitestIcon } from "@/assets/icons";
+import { TestComponentProps } from "./types/TestComponent.types";
 
-const TestComponent: React.FC = () => {
+/**
+ * This component is a test component for the boilerplate
+ * It contains a few buttons and a counter that increments and decrements
+ * It also displays the value of the counter
+ * The state of the counter is managed by the Redux store
+ * The buttons are connected to the Redux store using the useDispatch hook
+ * The value of the counter is displayed using the useSelector hook
+ */
+const TestComponent: React.FC<TestComponentProps> = () => {
 
     const { value } = useSelector((state: RootState) => state.counter);
     const dispatch = useDispatch()
@@ -11,12 +22,13 @@ const TestComponent: React.FC = () => {
     return ( 
         <>
             <h1>BoilerPlate by LilLakes</h1>
-
-            <ul>
-                <li>ReactJs</li>
-                <li>Typescript</li>
-                <li>Vite</li>
-                <li>Redux - Redux Saga</li>
+            
+            <ul className="no_point_ul">
+                <li className="icon_label"><img src={reactIcon} alt="icon" className="icon"/>ReactJs</li>
+                <li className="icon_label"><img src={typescriptIcon} alt="icon" className="icon"/>Typescript</li>
+                <li className="icon_label"><img src={viteIcon} alt="icon" className="icon"/>Vite</li>
+                <li className="icon_label"><img src={reduxIcon} alt="icon" className="icon"/>Redux - Redux Saga</li>
+                <li className="icon_label"><img src={vitestIcon} alt="icon" className="icon"/>Vitest - React Testing Library</li>
             </ul>
 
             <div className="flex gap">
